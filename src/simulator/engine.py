f5a3229d8271d86b698311c64e8442e9ef79f764
Ríad Nassiffe <riad.nassiffe@gmail.com>
2021-04-16 21:02:05 -0300

    simulation tests with simpy

--- src/simulator/engine.py
+++ src/simulator/engine.py
@@ -1,5 +1,26 @@
-import core
+import simpy
 
 
-def main():
-    print("Hello")
+class School:
+    def __init__(self, env):
+        self.env = env
+        self.class_ends = env.event()
+        self.pupil_procs = [env.process(self.pupil()) for i in range(10)]
+        self.bell_proc = env.process(self.bell())
+
+    def bell(self):
+        for i in range(2):
+            yield self.env.timeout(45)
+            self.class_ends.succeed()
+            self.class_ends = self.env.event()
+            print()
+
+    def pupil(self):
+        for i in range(2):
+            yield self.class_ends
+            print(r' \o/', end='')
+
+
+env = simpy.Environment()
+school = School(env)
+env.run(until=46)
